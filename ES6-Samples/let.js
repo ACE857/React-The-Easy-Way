5777c10569032a597d6d121dac40585c58d4f079
ace857 <srjbsht857@gmail.com>
2020-01-05 11:37:06 +0530

    let-commit

---
+++ ES6-Samples/let.js
@@ -0,0 +1,17 @@
+console.log("without using let - ");
+
+var name = "Suraj"
+if (name) {
+var name = "Bisht"
+console.log('local scope - ', name)             // local scope
+}
+console.log('global scope - ', name)            // global scope
+
+
+console.log("\n\nusing let - ");
+var name = "Suraj"
+if (name) {
+let name = "Bisht"
+console.log('local scope - ', name) 
+}
+console.log('global scope - ', name) 
